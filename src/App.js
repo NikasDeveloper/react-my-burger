49da7946e1f0bb7d9b88cca65ae7c76859ef8ec3
Nikas <nikas.lebedenko@gmail.com>
2018-02-18 19:13:10 +0200

    Added Burger builder component + layout styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Aux from './hoc/Aux';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <Aux>
         <Layout>
-          <p>Hello world</p>
+          <BurgerBuilder />
         </Layout>
       </Aux>
     );
