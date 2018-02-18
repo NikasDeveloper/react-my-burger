9de3a1f96bd291dd857a659f36d66eacf260e816
Nikas <nikas.lebedenko@gmail.com>
2018-02-18 22:12:19 +0200

    Added dynamic burger ingredients rendering.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+  const separator = '-';
+  const ingredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, key) => {
+      return <BurgerIngredient key={igKey + separator + key} type={igKey} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="cheese" />
+      {ingredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
