3690146cd07e9c1d4d5ebe881d97880ec13b47be
Nikas <nikas.lebedenko@gmail.com>
2018-02-18 19:04:53 +0200

    Added layout component, Aux hoc.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import Aux from './hoc/Aux';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <Aux>
+        <Layout>
+          <p>Hello world</p>
+        </Layout>
+      </Aux>
+    );
   }
 }
 
