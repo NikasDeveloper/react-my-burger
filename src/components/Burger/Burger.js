bbb461fdb63c6d06f44623af0d827c1d3e944a4d
Nikas <nikas.lebedenko@gmail.com>
2018-02-18 23:05:48 +0200

    Flatted array of ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
   const separator = '-';
-  const ingredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, key) => {
-      return <BurgerIngredient key={igKey + separator + key} type={igKey} />;
-    });
-  });
+  let ingredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, key) => {
+        return <BurgerIngredient key={igKey + separator + key} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => [...arr, ...el], []);
+  console.log(ingredients);
+  if (ingredients.length === 0) ingredients = <p>Please start adding ingredients!</p>;
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
