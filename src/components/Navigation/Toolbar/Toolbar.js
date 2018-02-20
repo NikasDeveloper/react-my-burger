5820e4f020b95f5748a6dca90a14bedecd3b2dfa
Nikas <nikas.lebedenko@gmail.com>
2018-02-20 23:35:51 +0200

    Added dynamic side bar toogling.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Hamburger from '../SideDrawer/Hamburger/Hamburger';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <Hamburger clicked={props.openedSideDrawer} />
     <div className={classes.Logo}><Logo /></div>
     <nav className={classes.DesktopOnly}><NavigationItems /></nav>
   </header>
