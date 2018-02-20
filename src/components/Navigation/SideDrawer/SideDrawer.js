659182827e671c1d8a08c7c6595daa399480ce1d
Nikas <nikas.lebedenko@gmail.com>
2018-02-20 22:39:50 +0200

    Added navigation items + side drawer + restyled logo for mobile.

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const sideDrawer = props => {
+  //
+  return (
+    <div className={classes.SideDrawer}>
+      <div className={classes.Logo}><Logo /></div>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
