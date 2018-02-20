20501eb02e004964d7a9440ecdf245c5b86f7503
Nikas <nikas.lebedenko@gmail.com>
2018-02-21 00:07:36 +0200

    Finished project v1

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,9 +7,6 @@ class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     return nextProps.show !== this.props.show;
   }
-  componentWillUpdate() {
-    console.log('Updating modal');
-  }
   render() {
     const style = {
       transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
