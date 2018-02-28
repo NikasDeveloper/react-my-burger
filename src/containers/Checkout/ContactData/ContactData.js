3255f0248903e2d643c047bf193f5c65c1cc1344
Nikas <nikas.lebedenko@gmail.com>
2018-02-28 16:44:24 +0200

    Finished form module

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -57,7 +57,7 @@ class ContactData extends Component {
             { value: 'cheapest', display: 'Cheapest' }
           ]
         },
-        value: '',
+        value: 'fastest',
       },
     },
     loading: false
